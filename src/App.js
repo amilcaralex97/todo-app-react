4820e73c1ecf1917ec4bd61094aefb6e766f5962
Amílcar Alejandro Sánchez Soltero <amilcaralex97@gmail.com>
2019-07-16 13:00:10 -0500

    start

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  state={
+    todos:[
+      {id:1, content:'buy some milk'},
+      {id:2, content:'play mario kart'}
+    ]
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
