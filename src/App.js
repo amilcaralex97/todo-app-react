f967d978c4a7ea444da9fc67a1d0103d75797bb6
Amílcar Alejandro Sánchez Soltero <amilcaralex97@gmail.com>
2019-07-17 10:51:07 -0500

    delete todo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Todos from './Todos'
 
 export default class App extends Component {
   state={
@@ -7,10 +8,21 @@ export default class App extends Component {
       {id:2, content:'play mario kart'}
     ]
   }
+  
+  deleteTodo=(id)=>{
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    })
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className='todo-app container'>
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     )
   }
