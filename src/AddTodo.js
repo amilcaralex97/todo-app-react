712ab3912269b4be6540bd6a32fcbd404fcf2a13
Amílcar Alejandro Sánchez Soltero <amilcaralex97@gmail.com>
2019-07-17 12:41:32 -0500

    Final(fixed)

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -15,7 +15,7 @@ export default class AddTodo extends Component {
         e.preventDefault();
         this.props.addTodo(this.state)
         this.setState({
-            content:'',
+            content:''
         })
     }
 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 export default class App extends Component {
-  state={
+   state={
     todos:[
       {id:1, content:'buy some milk'},
       {id:2, content:'play mario kart'}
     ]
   }
-  
-  deleteTodo=(id)=>{
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id
-    })
+
+  addTodo = (todo) =>{
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
     this.setState({
       todos
     })
@@ -23,8 +23,8 @@ export default class App extends Component {
       <div className='todo-app container'>
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     )
   }
 }
-
