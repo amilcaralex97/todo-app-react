db248f4c13af6da3d78c0b6f67f2f4ad73c38766
Amílcar Alejandro Sánchez Soltero <amilcaralex97@gmail.com>
2019-07-17 12:33:23 -0500

    Revert "finish-app-todo"
    This reverts commit d54f3179db2882024fdf4cb91098b1e84af259d2.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Todos from './Todos'
-import AddTodo from './AddTodo'
 
 export default class App extends Component {
   state={
@@ -19,20 +18,11 @@ export default class App extends Component {
     })
   }
 
-  addTodo = (todo) =>{
-    todo.id = Math.random()
-    let todos = [...this.state.todos, todo]
-    this.setState({
-      todos
-    })
-  }
-
   render() {
     return (
       <div className='todo-app container'>
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
-        <AddTodo addTodo={this.addTodo}/>
       </div>
     )
   }
